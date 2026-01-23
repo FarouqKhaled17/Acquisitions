3d19e83dc8a536c89f67c08f5bf69ccb9067fce9
FarouqKhaled17 <faroukkhaled45@gmail.com>
2026-01-23 04:00:24 +0200

    feat: add Neon + Drizzle DB setup, user model, and tooling updates

---
+++ src/models/user.model.js
@@ -0,0 +1,11 @@
+import { pgTable,serial, timestamp, varchar } from 'drizzle-orm/pg-core';
+
+export const users=pgTable('users',{
+  id:serial('id').primaryKey(),
+  name:varchar('name',{length:100}).notNull(),
+  email:varchar('email',{length:255}).notNull().unique(),
+  passwordHash:varchar('password_hash',{length:255}).notNull(),
+  createdAt:timestamp('created_at').defaultNow().notNull(),
+  updatedAt:timestamp('updated_at').defaultNow().notNull(),
+  deletedAt:timestamp('deleted_at').defaultNow().notNull(),
+});
